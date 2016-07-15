cbd8f3dd43f4595c440c53faac5a0d2614ed75ee
Joe Reynolds <joereynolds952@gmail.com>
2016-07-15 22:11:02 +0100

    try and fail to package it

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="silver-doodle",
+    description="A linter for the Scheme programming language",
+    packages=find_packages()
+)
--- silver_doodle_package/test.py
+++ silver_doodle_package/test.py
@@ -7,7 +7,7 @@ import messages
 class TestLinter(unittest.TestCase):
 
     def test_it_finds_the_errors(self):
-        linter = lint.Linter('test-file.ss')
+        linter = lint.Linter('tests/scheme-files/test-file.ss')
         errors = linter.get_errors()
 
         self.assertIn(messages.message['tabs'], errors[0])
@@ -17,7 +17,7 @@ class TestLinter(unittest.TestCase):
         self.assertIn(messages.message['blank_line'], errors[4])
 
     def test_it_reports_the_correct_line_number(self):
-        linter = lint.Linter('test-file.ss')
+        linter = lint.Linter('tests/scheme-files/test-file.ss')
         errors = linter.get_errors()
 
         line_number = 1
@@ -26,11 +26,11 @@ class TestLinter(unittest.TestCase):
         self.assertEqual(errors[2][line_number], 8)
 
     def test_the_file_content_gets_populated(self):
-        linter = lint.Linter('test-file.ss')
+        linter = lint.Linter('tests/scheme-files/test-file.ss')
         self.assertGreater(len(linter.file_content), 1)
 
     def test_it_gets_the_content_for_a_given_line(self):
-        linter = lint.Linter('test-file.ss')
+        linter = lint.Linter('tests/scheme-files/test-file.ss')
         content = linter.get_content_for_line(8)
         self.assertEqual(content, ')\n')
 
@@ -44,10 +44,10 @@ class TestLinter(unittest.TestCase):
         try:
             out = StringIO()
             sys.stdout = out
-            linter = lint.Linter('test-file.ss')
+            linter = lint.Linter('tests/scheme-files/test-file.ss')
             linter.show_errors()
             output = out.getvalue().strip()
-            self.assertIn('Tabs are not needed', output) 
+            self.assertIn('Tabs are not needed', output)
         finally:
             sys.stdout = saved_stdout
 
